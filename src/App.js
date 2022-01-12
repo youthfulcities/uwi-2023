96dc15a4cfe3d11deb5d34519d6eec3c183c66a4
Genna Weber <gennamweber@gmail.com>
2022-01-12 17:20:09 -0500

    Adds routes and sets up social links

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const dataset = "index";
+import Widgets from "./components/Widgets.js";
+import Home from "./pages/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <ods-dataset-context
-        context={dataset}
-        index-domain="https://pivothub.youthfulcities.com/"
-        index-dataset="index-2020-full"
-        index-urlsync="true"
-      >
-        <ods-facets context="index">
-          <ods-facet name="city_cma" disjunctive="true"></ods-facet>
-        </ods-facets>
-
-        <ods-map context={dataset}></ods-map>
-        <ods-chart align-month="true">
-          <ods-chart-query
-            context={dataset}
-            field-x="city"
-            maxpoints="15"
-            sort="serie1-1"
-          >
-            <ods-chart-serie
-              expression-y="value"
-              label-y="value (averaged)"
-              chart-type="column"
-              function-y="AVG"
-              color="#4F66AF"
-              display-values="false"
-              display-stack-values="false"
-              scientific-display="true"
-              color-thresholds='[{"color":"#EE605B","value":100},{"color":"#884C41","value":50},{"color":"#F7BCB7","value":25},{"color":"#B8D98D","value":15},{"color":"#4F66AF","value":10},{"color":"#FBD166","value":1}]'
-            ></ods-chart-serie>
-          </ods-chart-query>
-        </ods-chart>
-        <button onClick={() => window.history.push("/")}>Reset</button>
-      </ods-dataset-context>
-    </div>
+    <Router>
+      <h2>Nav Bar</h2>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/info/" element={<Widgets />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Socials.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailIcon,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  HatenaIcon,
+  InstapaperIcon,
+  LineIcon,
+  LinkedinIcon,
+  LivejournalIcon,
+  MailruIcon,
+  OKIcon,
+  PinterestIcon,
+  PocketIcon,
+  RedditIcon,
+  TelegramIcon,
+  TumblrIcon,
+  TwitterIcon,
+  ViberIcon,
+  VKIcon,
+  WeiboIcon,
+  WhatsappIcon,
+  WorkplaceIcon,
+} from "react-share";
+
+const Socials = () => {
+  return <></>;
+};
+
+export default Socials;

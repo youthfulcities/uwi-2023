8e75c5c1f5adfe6f13e1324ab5719ea015c57351
Genna <gennamweber@gmail.com>
2023-04-17 10:52:16 -0400

    Remove fade up animation quiz page

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -2,7 +2,6 @@ import { Box, Button, Container, Grid, Typography } from '@mui/material';
 import React, { useCallback, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import BasicContainer from '../components/BasicContainer';
-import FadeInUp from '../components/FadeInUp';
 import PhotoBackground from '../components/PhotoBackground';
 import RoundSymbolButton from '../components/RoundSymbolButton';
 import topics from '../data/topics.json';
@@ -39,74 +38,72 @@ const Quiz = ({
   return (
     <>
       <PhotoBackground />
-      {items && (
-        <FadeInUp>
-          <BasicContainer
-            languages={languages}
-            setCurrentLangCode={setCurrentLangCode}
-            currentLangCode={currentLangCode}>
-            <Typography variant="h5" mb={2}>
-              Select the aspects of a city that are{' '}
-              <span className="highlight">important to you.</span> Tap again to
-              deselect.
-            </Typography>
-            <Typography variant="body1" mb={4}>
-              Tap and hold on mobile to open the tooltip and learn more about
-              each topic.
-            </Typography>
-            {items.map((item) => (
-              <RoundSymbolButton
-                index={item.id}
-                key={item.key}
-                topic={item.key}
-                desc={item.desc}
-                included={item.included}
-                name={item.name}
-                handleClick={handleClick}
-              />
-            ))}
-          </BasicContainer>
-          <Box
-            sx={{
-              // background:
-              //   'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%)',
-              // position: 'sticky',
-              bottom: 0,
-            }}
-            pb={2}>
-            <Container maxWidth="lg">
-              <Grid
-                sx={{ minHeight: '10vh', maxHeight: '10vh' }}
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center">
-                <Grid item mx={1} mb={1}>
-                  <Link to="/">
-                    <Button variant="contained" color="primary">
-                      Home
-                    </Button>
-                  </Link>
-                </Grid>
-                <Grid item mx={1} mb={1}>
-                  {items.some((e) => e.included === true) ? (
-                    <Button
-                      variant="contained"
-                      color="info"
-                      onClick={(e) => handleSubmit(e)}>
-                      Show me my results
-                    </Button>
-                  ) : (
-                    <Button variant="contained" color="info" disabled>
-                      Show me my results
-                    </Button>
-                  )}
-                </Grid>
+      <>
+        <BasicContainer
+          languages={languages}
+          setCurrentLangCode={setCurrentLangCode}
+          currentLangCode={currentLangCode}>
+          <Typography variant="h5" mb={2}>
+            Select the aspects of a city that are{' '}
+            <span className="highlight">important to you.</span> Tap again to
+            deselect.
+          </Typography>
+          <Typography variant="body1" mb={4}>
+            Tap and hold on mobile to open the tooltip and learn more about each
+            topic.
+          </Typography>
+          {items.map((item) => (
+            <RoundSymbolButton
+              index={item.id}
+              key={item.key}
+              topic={item.key}
+              desc={item.desc}
+              included={item.included}
+              name={item.name}
+              handleClick={handleClick}
+            />
+          ))}
+        </BasicContainer>
+        <Box
+          sx={{
+            // background:
+            //   'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%)',
+            // position: 'sticky',
+            bottom: 0,
+          }}
+          pb={2}>
+          <Container maxWidth="lg">
+            <Grid
+              sx={{ minHeight: '10vh', maxHeight: '10vh' }}
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center">
+              <Grid item mx={1} mb={1}>
+                <Link to="/">
+                  <Button variant="contained" color="primary">
+                    Home
+                  </Button>
+                </Link>
               </Grid>
-            </Container>
-          </Box>
-        </FadeInUp>
-      )}
+              <Grid item mx={1} mb={1}>
+                {items.some((e) => e.included === true) ? (
+                  <Button
+                    variant="contained"
+                    color="info"
+                    onClick={(e) => handleSubmit(e)}>
+                    Show me my results
+                  </Button>
+                ) : (
+                  <Button variant="contained" color="info" disabled>
+                    Show me my results
+                  </Button>
+                )}
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </>
     </>
   );
 };

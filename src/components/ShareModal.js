328f99d650b230c6115b6601e3f692e46a7b2d30
Genna <gennamweber@gmail.com>
2023-06-28 17:58:48 -0400

    Add share and download functions

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -1,6 +1,16 @@
-import { Backdrop, Button, Fade, Modal, Paper } from '@mui/material';
+import ShareIcon from '@mui/icons-material/Share';
+import {
+  Backdrop,
+  Button,
+  Fade,
+  Grid,
+  IconButton,
+  Modal,
+  Paper
+} from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { saveAs } from 'file-saver';
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
@@ -33,14 +43,56 @@ const ShareModal = () => {
     borderRadius: '12px',
   };
 
+  const url = window.location.origin;
+  const title = 'Title';
+  const text = 'Text';
+
+  const handleDownload = () => {
+    saveAs(`./assets/images/share/${city}.png`, `best-work-city-${city}.png`);
+  };
+
+  const handleSharing = async () => {
+    const response = await fetch(`./assets/images/share/${city}.png`);
+    const blob = await response.blob();
+
+    const filesArray = [
+      new File([blob], `${city}.png`, {
+        type: 'image/jpeg',
+        lastModified: new Date().getTime(),
+      }),
+    ];
+
+    const shareDetails = {
+      files: filesArray,
+      url,
+      title,
+      text,
+    };
+
+    if (navigator.share) {
+      try {
+        await navigator
+          .share(shareDetails)
+          .then(() =>
+            console.log('Hooray! Your content was shared to tha world')
+          );
+      } catch (error) {
+        console.log(`Oops! I couldn't share to the world because: ${error}`);
+      }
+    } else {
+      handleDownload();
+      console.log('Web share is currently not supported on this browser.');
+    }
+  };
+
   return (
     <>
       <Button variant="contained" color="info" onClick={handleOpen}>
         Share my results
       </Button>
       <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
+        aria-labelledby="Share image popup"
+        aria-describedby={`Share image of ${city}`}
         open={open}
         onClose={handleClose}
         closeAfterTransition
@@ -57,6 +109,24 @@ const ShareModal = () => {
               src={`./assets/images/share/${city}.png`}
               alt=""
             />
+            <Grid mt={1} container>
+              <Grid item mr={1}>
+                <IconButton
+                  onClick={handleSharing}
+                  size="large"
+                  sx={{ backgroundColor: '#fff' }}>
+                  <ShareIcon fontSize="large" />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton
+                  onClick={handleDownload}
+                  size="large"
+                  sx={{ backgroundColor: '#fff' }}>
+                  <ShareIcon fontSize="large" />
+                </IconButton>
+              </Grid>
+            </Grid>
           </Paper>
         </Fade>
       </Modal>
